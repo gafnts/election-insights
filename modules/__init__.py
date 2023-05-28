b2a015176010fba9b8c3458f4b7ac0aa8336bbbe
Gabriel Fuentes <83990480+gafnts@users.noreply.github.com>
2023-05-27 18:28:05 -0600

    Renaming classes

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,3 @@
 from .logger_config import setup_logger
-from .twitter_request import GetTweets
-from .openai_request import GetGPTFeatures
+from .twitter_request import TwitterRequest
+from .openai_request import OpenAIRequest
